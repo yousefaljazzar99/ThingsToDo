6203ed455956234af4ec705c58c1d13a96b674f3
yousefaljazzar99 <joseph.ana.19221@gmail.com>
2020-12-04 00:12:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/thingstodo/screen2.java
@@ -0,0 +1,14 @@
+package com.example.thingstodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+}
